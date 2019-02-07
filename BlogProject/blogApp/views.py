53132fb1ef3ff18d392c4d5139a0b633a57d2745
hyper-vision <exile744@live.com>
2019-02-07 13:58:14 +0530

    Created user authentication and login app

--- BlogProject/blogApp/views.py
+++ BlogProject/blogApp/views.py
@@ -1,33 +1,10 @@
 from django.shortcuts import render
-from django.shortcuts import render
-
-# Create your views here.
-
-posts = [
-            {
-                'title':'Blog Post 1',
-                'author':'Red',
-                'content':'This is my first blog post!',
-                'date_posted':'12-12-2018'
-            },
-            {
-                'title':'Blog Post 2',
-                'author':'Red',
-                'content':'This is my second blog post!',
-                'date_posted':'03-01-2019'
-            },
-            {
-                'title':'Blog Post 3',
-                'author':'Red',
-                'content':'This is my third blog post!',
-                'date_posted':'12-01-2019'
-            }
-        ]
+from .models import Post 
 
 home_context = {
-    'posts':posts,
+    'posts': Post.objects.all(),
     'title':'Home'
-}
+} 
 
 def home(request):
     return render(request, 'blogApp/home.html', context=home_context)
